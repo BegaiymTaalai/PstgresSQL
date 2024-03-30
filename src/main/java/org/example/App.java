6813e80d25f2de53c130620f656d5021c1ca697b
meder <meder.tynychbekov@gmail.com>
2024-03-30 23:50:50 +0600

    добавил пару горячих клавиш

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,12 +10,20 @@ import java.util.List;
  */
 public class App {
     public static void main(String[] args) {
+
+        /**
+         * command || control + { -> возвращает назад
+         command || contor + ЛКМ(левая кнопка мыши) -> открывает реализацию класса или метода
+         */
         UserService userService = new UserService();
 
         userService.createTable();
 //        userService.saveUser(new User("Aida", "Zheenbaeva"));
 
+
         userService.updateById(11, new User("Aida", "Baatyrbekovna"));
+        userService.deleteById(2);
+        userService.updateById(2, new User("dlf", "dfs"));
 
 
     }
